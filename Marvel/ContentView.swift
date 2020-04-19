b3d78fc2a694a2572d6634c66c13f30ec58e1b19
Asgedom <asmow@yahoo.com>
2020-04-19 12:38:39 -0500

    Initial Commit

---
+++ Marvel/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Marvel
+//
+//  Created by Asge Yohannes on 4/19/20.
+//  Copyright © 2020 Asge Yohannes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
